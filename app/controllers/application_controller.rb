4b288cb3286cf9041963a314db5d907f98c60dcc
ddgriffin <dianadgriffin@gmail.com>
2014-12-18 21:04:10 -0600

    Redirect back to the page you were on after log in!!!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,39 +10,54 @@ class ApplicationController < ActionController::Base
 
   after_filter :store_location
 
-  def store_location
-   if (!request.fullpath.match("/users") &&
-    !request.xhr?) # don't store ajax calls
-    session[:previous_url] = request.fullpath
-   end
-  end
+  #THIS WAS ALL IN THE FILE PREVIOUS TO SHEHACKS
+      # def store_location
+      #  if (!request.fullpath.match("/users") &&
+      #   !request.xhr?) # don't store ajax calls
+      #   session[:previous_url] = request.fullpath
+      #  end
+      # end
 
-  #def after_sign_in_path_for(resource)
-  #  if Rails.env.production?
-  #    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'https')
-  #  else
-  #    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
-  #  end
-  #  if request.referer == sign_in_url
-  #    session["user_return_to"] || root_path
-  #  else
-  #    stored_location_for(resource) || request.referer || root_path
-  #  end
-  #end
+      #def after_sign_in_path_for(resource)
+      #  if Rails.env.production?
+      #    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'https')
+      #  else
+      #    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+      #  end
+      #  if request.referer == sign_in_url
+      #    session["user_return_to"] || root_path
+      #  else
+      #    stored_location_for(resource) || request.referer || root_path
+      #  end
+      #end
+
+      # If your model is called User
+      # def after_sign_in_path_for(resource)
+      #   session["user_return_to"] || root_path
+      # end
 
   # If your model is called User
-  def after_sign_in_path_for(resource)
-    session["user_return_to"] || root_path
+def store_location
+  # store last url - this is needed for post-login redirect to whatever the user last visited.
+  return unless request.get?
+  if (request.path != "/users/sign_in" &&
+      !request.xhr?) # don't store ajax calls
+    session[:previous_url] = request.fullpath
   end
+end
 
-  private
-  def render_error(status, exception)
-    respond_to do |format|
-      formatted_exception = "== #{exception.message} \n #{exception.backtrace}"
-      puts formatted_exception
-      logger.error(formatted_exception)
-      format.html { render template: "errors/error_#{status}", layout: 'layouts/application', status: status }
-      format.all { render nothing: true, status: status }
-    end
+def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end
+
+private
+def render_error(status, exception)
+  respond_to do |format|
+    formatted_exception = "== #{exception.message} \n #{exception.backtrace}"
+    puts formatted_exception
+    logger.error(formatted_exception)
+    format.html { render template: "errors/error_#{status}", layout: 'layouts/application', status: status }
+    format.all { render nothing: true, status: status }
   end
+end
 end
