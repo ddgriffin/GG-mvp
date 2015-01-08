453249f7a7cf22aae3868cdb01eecb21abd22b0f
Cheyenne Weaver <cheyenneweaver@gmail.com>
2015-01-07 20:24:14 -0600

    Create Houston page and add City field on X-edit by neighborhood

--- app/assets/javascripts/events.js.coffee
+++ app/assets/javascripts/events.js.coffee
@@ -5,3 +5,11 @@ jQuery ->
     $(this).toggleClass("fa-star-o")
     $(this).toggleClass("fa-star")
     $.post $(this).data("url")
+
+jQuery ->
+  $(".filter_austin").click ->
+    $("#all_events").show()
+    $("#houston_events").hide()
+  $(".filter_houston").click ->
+    $("#houston_events").show()
+    $("#all_events").hide()
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,10 @@ class EventsController < ApplicationController
     @tba_events = Event.where( datetime_tba: true, state: ['accepted']).sort_by { |e| e.created_at }
     @filled_events = Event.where( datetime_tba: false, state: ['filled']).sort_by { |e| e.begins_at }.reverse!
     @closed_events = Event.where( datetime_tba: false, state: ['completed']).sort_by { |e| e.begins_at }.reverse!
+
+    @houston_events = Event.where( location_city: "Houston", state: 'accepted').sort_by { |e| e.created_at }.reverse!
+    @filled_houston_events = Event.where( location_city: "Houston", state: 'filled').sort_by { |e| e.begins_at }.reverse!
+    @closed_houston_events = Event.where( location_city: "Houston", state: 'completed').sort_by { |e| e.begins_at }.reverse!
   end
 
 end
