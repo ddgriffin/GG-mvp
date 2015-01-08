e6d82ed9fd13339ff2ba3a91456e253dcead048a
ddgriffin <dianadgriffin@gmail.com>
2015-01-07 22:23:58 -0600

    got queries searching for H/houston (case insensitive)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,9 +6,10 @@ class EventsController < ApplicationController
     @filled_events = Event.where( datetime_tba: false, state: ['filled']).sort_by { |e| e.begins_at }.reverse!
     @closed_events = Event.where( datetime_tba: false, state: ['completed']).sort_by { |e| e.begins_at }.reverse!
 
-    @houston_events = Event.where( location_city: "Houston", state: 'accepted').sort_by { |e| e.created_at }.reverse!
-    @filled_houston_events = Event.where( location_city: "Houston", state: 'filled').sort_by { |e| e.begins_at }.reverse!
-    @closed_houston_events = Event.where( location_city: "Houston", state: 'completed').sort_by { |e| e.begins_at }.reverse!
+    t = Event.arel_table
+    @houston_events = Event.where(t[:location_city].matches("%Houston").and(t[:state].matches('accepted'))).sort_by { |e| e.created_at }.reverse!
+    @filled_houston_events = Event.where(t[:location_city].matches("%Houston").and(t[:state].matches('filled'))).sort_by { |e| e.begins_at }.reverse!
+    @closed_houston_events = Event.where(t[:location_city].matches("%Houston").and(t[:state].matches('completed'))).sort_by { |e| e.begins_at }.reverse!
   end
 
 end
